ecce67935dd83af5200e42fb19eb00a515f62372
Ron M <ramayer+git@gmail.com>
2025-01-21 01:43:32 -0800

    avoid ERROR tests/test_on_existing_audio_classifier.py - torch.OutOfMemoryError: CUDA out of memory. Tried to allocate 20.00 MiB. GPU 0 has a total capacity of 5.78 GiB of which...

--- tests/test_audio_file_visualizer.py
+++ tests/test_audio_file_visualizer.py
@@ -1,9 +1,10 @@
-from audio_classifier_visualizer.audio_file_visualizer import AudioFileVisualizer
-
 import pytest
 
+from audio_classifier_visualizer.audio_file_visualizer import AudioFileVisualizer
+
+
 @pytest.mark.xfail(reason="Test file not yet added to repository")
-#@pytest.mark.parametrize("audio_file", ["tests/data/test.wav"])
+# @pytest.mark.parametrize("audio_file", ["tests/data/test.wav"])
 def test_can_create_audio_file_visualizer(audio_file: str = "tests/data/test.wav"):
     visualizer = AudioFileVisualizer(audio_file)
     assert visualizer is not None
